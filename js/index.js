b0186a6f670c5867926589a2a4b47f2bab634a29
Upendra Neupane <upendraneupane344@gmail.com>
2024-06-25 18:34:28 +0545

    upload website

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed(".auto-type",{
+    strings : ["Software Developer", "Graphic Designer", "Web Developer",  "UI/UX Designer"],
+    typeSpeed : 150,
+    backSpeed : 150,
+    loop : true
+});     
+                    
