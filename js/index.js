6fd7f0a328ea25250c1c75e820f64dcdca0557f7
Upendra Neupane <upendraneupane344@gmail.com>
2025-02-16 14:54:21 +0545

    Update

--- js/index.js
+++
@@ -1,8 +0,0 @@
-
-var typed = new Typed(".auto-type",{
-    strings : ["Software Developer", "Graphic Designer", "Web Developer",  "UI/UX Designer"],
-    typeSpeed : 80,
-    backSpeed : 80,
-    loop : true
-});     
-                    
